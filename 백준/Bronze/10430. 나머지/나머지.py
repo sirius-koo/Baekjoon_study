e39e9ec10e98b2901336e0a5d6d893be5da45708
Namseoku <36871379+namsuk@users.noreply.github.com>
2022-03-31 22:48:01 +0900

    Update 나머지.py

--- 백준/Bronze/10430. 나머지/나머지.py
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -1,5 +1,18 @@
+#map() 함수를 이용한 풀이방법
 A, B, C = map(int, input().split())
 print((A + B) % C)
 print(((A % C) + (B % C)) % C)
 print((A * B) % C)
 print(((A % C) * (B % C)) % C)
+
+'''
+다른 풀이
+A, B, C = input().split()
+A = int(A)
+B = int(B)
+C = int(C)
+print((A + B) % C)
+print(((A % C) + (B % C)) % C)
+print((A * B) % C)
+print(((A % C) * (B % C)) % C)
+'''

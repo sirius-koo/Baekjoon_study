30a9e1a751a4cd7f7a33957ccac40bc0f93e3a55
Namseoku <36871379+namsuk@users.noreply.github.com>
2022-04-01 23:51:25 +0900

    Update coding_ing.py

--- 백준/Bronze/coding_ing.py
+++ 백준/Bronze/coding_ing.py
@@ -1,9 +1,12 @@
-#백준 기다리는중 오류로 인한 코드 저장
-#BOJ_11021
+#BOJ_11022
+#A + B 구현을 어떻게 해야할지 고민중!
 T = int(input())
 sum_lst = []
 for i in range(0, T):
     A, B = map(int, input().split())
+    A_lst = A.split()
+    B_lst = B.split()
     sum_lst.append(A+B)
 for i in range(0, T):
-    print("Case #{}:".format(i + 1), sum_lst[i])
+    print("Case #{}:".format(i + 1), A_lst[i], "+", B_lst[i], ":", sum_lst[i])
+...

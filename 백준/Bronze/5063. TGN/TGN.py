641c25b2bfba3cb702b59f0c8f0e56963d1b13ca
Namseoku <36871379+sirius-koo@users.noreply.github.com>
2022-10-04 21:11:40 +0900

    [Bronze III] Title: TGN, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,11 @@
+import sys
+
+N = int(sys.stdin.readline())
+for i in range(N):
+    r, e, c = map(int, sys.stdin.readline().split())
+    if (r == (e - c)):
+        print("does not matter")
+    elif (r > (e - c)):
+        print("do not advertise")
+    elif (r < (e - c)):
+        print("advertise")

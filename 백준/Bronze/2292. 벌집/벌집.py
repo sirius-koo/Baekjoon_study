732c1b74aafe513248fa59f89c981e4412fc6805
Namseoku <36871379+sirius-koo@users.noreply.github.com>
2022-10-25 15:24:43 +0900

    Update 벌집.py

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,10 +1,10 @@
 import sys
 N = int(sys.stdin.readline())
 cnt = 1 # 시작점
-cnt_room = 6
+cnt_room = 6 # 지나가야 하는 방의 개수가 6의 배수
 cnt_min = 1 
 while N > cnt:
-    cnt_min += 1
-    cnt += cnt_room
+    cnt_min += 1 # 최소 개수로 방을 지나가는 방법
+    cnt += cnt_room 
     cnt_room += 6
 print(cnt_min)

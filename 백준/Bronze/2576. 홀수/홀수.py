1b8fa56b995fa1b54ab970775f5a027df2d83cc8
Namseoku <36871379+sirius-koo@users.noreply.github.com>
2022-10-25 22:09:16 +0900

    Update 홀수.py

--- 백준/Bronze/2576. 홀수/홀수.py
+++ 백준/Bronze/2576. 홀수/홀수.py
@@ -9,6 +9,6 @@ for i in range(7):
         even.append(num)
 if (odd == []):
     print(-1)
-elif (len(odd) >= 1):
+elif (len(odd) >= 1): # len(odd) > 1의 반례 : 홀수가 1개일 때, if (odd == [])에서 홀수가 없을 경우를 제외했으니 '>='를 사용하는게 맞다..
     print(sum(odd))
     print(min(odd))

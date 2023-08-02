35e4d7bb87a4271ae1d7914ddf5b6b1752070e98
Namseoku <36871379+sirius-koo@users.noreply.github.com>
2023-08-02 15:24:53 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,8 +1,3 @@
 def solution(s):
-    num_list = s.split(' ')
-    for i in range(len(num_list)):
-        num_list[i] = int(num_list[i])
-    max_num = str(max(num_list))
-    min_num = str(min(num_list))
-    return min_num + ' ' + max_num
-    
+    s = list(map(int, s.split(' ')))
+    return str(min(s)) + ' ' + str(max(s))

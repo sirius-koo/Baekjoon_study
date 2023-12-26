093eaf1e01586c4d88f821fe2b5f59ae56a3f68e
Namseoku <36871379+sirius-koo@users.noreply.github.com>
2023-12-26 21:48:26 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,7 +1,6 @@
 def solution(numbers):
-    if sum(numbers) == 0:
-        return '0'
-    
+    # if sum(numbers) == 0:
+    #     return '0'
     num = list(map(str, numbers))
     num.sort(key=lambda x:x*3, reverse=True)
-    return ''.join(num)
+    return str(int(''.join(num)))
